d781907ae62ddf0e2f267f021050d7ad57b4c6f3
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2021-11-24 18:35:07 +0100

    Test DataGrid View

--- SkiAreaApp/Form1.Designer.cs
+++ SkiAreaApp/Form1.Designer.cs
@@ -29,12 +29,69 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.skiAreaDBDataSet = new SkiAreaApp.SkiAreaDBDataSet();
+            this.skiAreaDBDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.skiAreaDBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.skiAreaDBDataSetBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.DataSource = this.skiAreaDBDataSetBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(775, 396);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // skiAreaDBDataSet
+            // 
+            this.skiAreaDBDataSet.DataSetName = "SkiAreaDBDataSet";
+            this.skiAreaDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // skiAreaDBDataSetBindingSource
+            // 
+            this.skiAreaDBDataSetBindingSource.DataSource = this.skiAreaDBDataSet;
+            this.skiAreaDBDataSetBindingSource.Position = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(713, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "View";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.skiAreaDBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.skiAreaDBDataSetBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource skiAreaDBDataSetBindingSource;
+        private SkiAreaDBDataSet skiAreaDBDataSet;
+        private System.Windows.Forms.Button button1;
     }
 }
 
